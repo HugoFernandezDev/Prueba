a5e9ba9ca8566763fd544f798b2f7e7c156ff7d0
HugoFernandezDev <hugo.fernandez@vallegrande.edu.pe>
2025-11-23 21:50:22 +0000

    h

--- config.py
+++
@@ -1,7 +0,0 @@
-DB_CONFIG = {
-    'host': 'mysql.czkfpua1vozt.us-east-1.rds.amazonaws.com',
-    'user': 'admin',
-    'password': 'Hadrianf1234',
-    'database': 'sumak_mikuy',
-    'port': 3306
-}
